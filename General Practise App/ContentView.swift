1b2d4d2f11c021fc860cb21f2258fdd4e0fb8837
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-30 22:59:49 +0530

    2st commit

--- General Practise App/ContentView.swift
+++ General Practise App/ContentView.swift
@@ -7,22 +7,74 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Button("Press me") {
-            print(type(of: self.body))
-        }
-        .padding()
-        .foregroundStyle(.white)
-        .background(Color.red)
-        .padding()
-        .background(Color.green)
-        .padding()
-        .background(Color.blue)
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .font(.largeTitle.weight(.heavy))
+            .foregroundStyle(.white)
+            .background(.indigo)
+            .clipShape(RoundedRectangle(cornerRadius: 6.0))
+            .shadow(radius: 12.0)
+    }
+}
 
+extension View {
+    func customTitleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+
+struct GridStack<Content:View> : View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int,Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row,column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct ContentView: View {
+    @State private var usedRedText = false
+    var body: some View {
+        VStack {
+            //            CustomTextView(text: "My name is Shubham")
+            //            CustomTextView(text: "Age 24")
+            //            CustomTextView(text: "Working to get job")
+            //            Text("Hello World")
+            //                .customTitleStyle()
+            GridStack(rows: 4, columns: 3) { row, col in
+                    Image(systemName: "\(row * 4 + col).circle")
+                    Text("R\(row) C\(col)")
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CustomTextView: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .padding()
+            .background(Color.red)
+            .foregroundStyle(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 6.0))
+            .shadow(radius: 10)
+    }
 }
