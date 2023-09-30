0227535ddbeaed90effaccd6a45fbf3b8939fe21
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-30 21:25:53 +0530

    Initial Commit

---
+++ General Practise App/General_Practise_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  General_Practise_AppApp.swift
+//  General Practise App
+//
+//  Created by Shubham on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct General_Practise_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
